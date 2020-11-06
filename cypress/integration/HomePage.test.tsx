3762c2e8bee41e9aa3e116fdb6ed029843e52833
Julius Dockwarder <hello@julius.dev>
2020-11-06 11:11:23 +0100

    test: extend home page test

--- cypress/integration/HomePage.test.tsx
+++ cypress/integration/HomePage.test.tsx
@@ -3,5 +3,12 @@ import { createYield } from "typescript"
 describe('Home Page', () => {
     it('loads without problems', () => {
         cy.visit('/')
+
+        cy.get('[data-cy=logo]').should('exist')
+
+        cy.get('[data-cy=learn-react-button]')
+            .should('exist')
+            .and('have.attr', 'href')
+            .and('eq', 'https://reactjs.org')
     })
 })
--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img data-cy="logo" src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,6 +15,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          data-cy="learn-react-button"
         >
           Learn React
         </a>
